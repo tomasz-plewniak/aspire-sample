11654640102ea576b593d9d4773d8edf191e911d
Tomasz Plewniak <t_plewniak@yahoo.com>
2024-01-31 09:22:45 +0100

    Sample project.

---
+++ AspireSample/AspireSample.AppHost/Program.cs
@@ -0,0 +1,11 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice");
+
+builder.AddProject<Projects.AspireSample_Web>("webfrontend")
+    .WithReference(cache)
+    .WithReference(apiService);
+
+builder.Build().Run();
