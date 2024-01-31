97bf813c6c2419732985ca78276a2351bf237a93
Tomasz Plewniak <t_plewniak@yahoo.com>
2024-01-31 14:01:28 +0100

    Add sql.

--- AspireSample/AspireSample.AppHost/Program.cs
+++ AspireSample/AspireSample.AppHost/Program.cs
@@ -1,11 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
-var postgresdb = builder.AddPostgres("pg")
+var sql = builder.AddSqlServerContainer("sq").AddDatabase("mssql");
+
+var postgresdb = builder.AddPostgresContainer("pg")
     .AddDatabase("postgresdb");
 
 var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice")
-    .WithReference(postgresdb);
+    .WithReference(postgresdb)
+    .WithReference(sql);
 
 builder.AddProject<Projects.AspireSample_Web>("webfrontend")
     .WithReference(cache)
